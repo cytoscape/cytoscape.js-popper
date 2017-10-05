f29d3dc6e4f06dbb8c1c56df725a95222187f8e8
Harsh Mistry <hnmistry@uwaterloo.ca>
2017-10-05 16:09:26 -0400

    Swapped Import for .require

--- src/core/render.js
+++ src/core/render.js
@@ -1,7 +1,14 @@
 //Include helper functions and Popper
-import Popper from 'popper.js';
+//import Popper from 'popper.js';
 const createBoundingBox = require('./createBoundingBox.js');
 const createReferenceObject = require('./createReferenceObject.js');
+
+//Fix Popper.js webpack import conflict (Use .default if using webpack)
+let Popper = require('popper.js');
+let EsmWebpackPopper = Popper.default;
+if (EsmWebpackPopper != null && EsmWebpackPopper.Defaults != null) {
+    Popper = Popper.default;
+}
 
 //Create a new popper object associated with a cytoscape element (Nodes or Edges)
 module.exports.createPopperObject = function (cyElement, userOptions) {
@@ -31,6 +38,7 @@ module.exports.createPopperObject = function (cyElement, userOptions) {
         //Stop creating a popper
         return;;
     }
+    console.log(Popper);
 
     //Create and return actual popper object
     var popper = new Popper(refObject, target, popperOpts);
